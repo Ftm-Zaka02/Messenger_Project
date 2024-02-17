aff88d5619ed3a0149ae142085ea6f0814cad35c
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-02-17 22:03:45 +0330

    feat: create seeder file for project

---
+++ database/seeders/MessagesTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Message;
+use Illuminate\Database\Seeder;
+
+class MessagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker=\Faker\Factory::create();
+        for ($i = 0; $i <= 10; $i++) {
+            Message::create([
+                'text_message'=>$faker->realText($maxNbChars = 100),
+                'send_time'=>$faker->unixTime($max = 'now'),
+                'user_id'=>191,
+                'chat_name'=>'farawin',
+                'deleted'=>0
+            ]);
+
+        }
+    }
+}
