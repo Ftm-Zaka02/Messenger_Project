b30e3780e97a2cd995193a1527df567ea52370d5
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-02-21 15:47:35 +0330

    build: create requst & factory files

---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Message;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
+ */
+class MessageFactory extends Factory
+{
+    protected $Message=Message::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+//        return [
+//            'text_message' => $this->faker->realText($maxNbChars = 100),
+//            'send_time' => $this->faker->unixTime($max = 'now'),
+//            'user_id' => 191,
+//            'chat_name' => 'farawin',
+//            'deleted' => 0
+//        ];
+    }
+}
