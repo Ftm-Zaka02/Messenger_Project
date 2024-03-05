c2b432d8e89114174218bd33c89ec3571222097c
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-03-05 21:48:33 +0330

    feat: create AuthController

---
+++ app/Http/Controllers/messenger/AuthController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\messenger;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Http\Requests\validator\LoginRequest;
+
+class AuthController extends Controller
+{
+   public static function login(LoginRequest $request)
+   {
+       $validated = $request->validated();
+   }
+}
