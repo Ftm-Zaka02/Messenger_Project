5631ec1163a38a5f3d43c4b4f4b36335ec1f79f8
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-03-08 18:31:40 +0330

    feat: set schedule for delete laravel.log

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\File;
 
 class Kernel extends ConsoleKernel
 {
@@ -15,6 +16,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->call(function () {
+            File::delete(storage_path('logs/laravel.log'));
+        })->daily()->cron('* * * * *');
         // $schedule->command('inspire')->hourly();
     }
 
