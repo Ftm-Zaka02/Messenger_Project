d667205781d8f202243e451194a3f2c3f6fe765c
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-03-14 09:41:15 +0330

    build: create notification table

--- app/Notifications/LoginConfirmation.php
+++ app/Notifications/LoginConfirmation.php
@@ -41,7 +41,7 @@ class LoginConfirmation extends Notification
     public function toMail($notifiable)
     {
         return (new MailMessage)
-                    ->line('Login was successful!.');
+                    ->line('The introduction to the notification.')
 //                    ->action('Notification Action', url('/'))
 //                    ->line('Thank you for using our application!');
     }
