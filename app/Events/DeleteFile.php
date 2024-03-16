68cd3ef2dcfcd4a4c8b65b069d9202fc41e46862
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-03-16 09:34:42 +0330

    feat: write DeleteFile event & listener

---
+++ app/Events/DeleteFile.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\messenger\Message;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class DeleteFile
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public $model;
+
+    /**
+     * Create a new event instance.
+     *
+     * @return void
+     */
+    public function __construct(Message $model)
+    {
+        $this->model=$model;
+    }
+
+    /**
+     * Get the channels the event should broadcast on.
+     *
+     * @return \Illuminate\Broadcasting\Channel|array
+     */
+    public function broadcastOn()
+    {
+        return new PrivateChannel('channel-name');
+    }
+}
