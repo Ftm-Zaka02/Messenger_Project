3add30c47062f0d2ff3d9d9cef978ded9f0496b2
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-03-20 14:10:54 +0330

    build: create ContactController

---
+++ app/Http/Controllers/messenger/ContactController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\messenger;
+
+use App\Http\Controllers\Controller;
+use App\Models\messenger\Contact;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+class ContactController extends Controller
+{
+    //
+}
