50485a8ae56d0719ea1748a8870f3d466648f265
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-03-22 15:09:52 +0330

    feat: write setContact method model

--- app/Models/messenger/Contact.php
+++ app/Models/messenger/Contact.php
@@ -8,6 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Contact extends Model
 {
     use HasFactory;
+
     protected $connection = "mysql";
     protected $table = 'contacts';
+    protected $fillable = ['phone', 'name','deleted_at'];
+
+    public static function setContact($phone,$name)
+    {
+        $model = self::create(['phone'=>$phone,'name'=>$name]);
+        return $model;
+    }
+
+    public static function getContact()
+    {
+
+    }
 }
