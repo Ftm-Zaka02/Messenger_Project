d071628ea2c3a20affac41a323f59638a21d5225
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-03-23 15:03:52 +0330

    feat: write getContact model

--- app/Models/messenger/Contact.php
+++ app/Models/messenger/Contact.php
@@ -11,16 +11,17 @@ class Contact extends Model
 
     protected $connection = "mysql";
     protected $table = 'contacts';
-    protected $fillable = ['phone', 'name','deleted_at'];
+    protected $fillable = ['phone', 'name', 'deleted_at'];
 
-    public static function setContact($phone,$name)
+    public static function setContact($phone, $name)
     {
-        $model = self::create(['phone'=>$phone,'name'=>$name]);
+        $model = self::create(['phone' => $phone, 'name' => $name]);
         return $model;
     }
 
     public static function getContact()
     {
-
+        $data = self::get();
+        return $data;
     }
 }
