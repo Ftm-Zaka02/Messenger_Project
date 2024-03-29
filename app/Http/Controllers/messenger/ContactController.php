a9819345d55ccae2ea503a85a169117f426bdab7
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-03-29 12:12:52 +0330

    feat: write updateContact method controller

--- app/Http/Controllers/messenger/ContactController.php
+++ app/Http/Controllers/messenger/ContactController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\messenger;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\validator\contacts\SetContactRequest;
+use App\Http\Requests\validator\contacts\UpdateContactRequest;
 use App\Models\messenger\Contact;
 use Illuminate\Support\Facades\Log;
 
@@ -11,7 +12,7 @@ class ContactController extends Controller
 {
     public static function set(SetContactRequest $request)
     {
-        $data=$request->validated();
+        $data = $request->validated();
         try {
             $model = Contact::setContact($data);
             $response = json_encode([
@@ -21,6 +22,27 @@ class ContactController extends Controller
             return response($response, 200);
         } catch (\Exception $error) {
             Log::error('setting contact got error: ' . $error->getMessage());
+            $response = json_encode([
+                'status' => 'error',
+                'message' => $error->getMessage(),
+            ]);
+            return response($response, 500);
+        }
+    }
+
+    public static function update(UpdateContactRequest $request)
+    {
+        $data = $request->validated();
+        try {
+            $model = Contact::updateContact($data);
+            $response = json_encode([
+                'status' => 'success',
+                'data' => $model,
+            ]);
+            return response($response, 200);
+
+        } catch (\Exception $error) {
+            Log::error('updating contact got error: ' . $error->getMessage());
             $response = json_encode([
                 'status' => 'error',
                 'message' => $error->getMessage(),
