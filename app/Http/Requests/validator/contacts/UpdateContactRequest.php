2cce80a4995981a7a1cb01b97e3e79b9b59290c6
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-03-29 12:05:52 +0330

    feat: validate updateContact request

---
+++ app/Http/Requests/validator/contacts/UpdateContactRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests\validator\contacts;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateContactRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'firstName' => 'required|max:50|string',
+            'lastName'=>'required|max:50|string'
+        ];
+    }
+}
