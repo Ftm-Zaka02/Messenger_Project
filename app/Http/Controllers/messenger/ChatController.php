9cbacf2c09bf6a96ed2322998c0146c1072bbb56
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-04-09 21:31:52 +0330

    build: create chat controller

---
+++ app/Http/Controllers/messenger/ChatController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\messenger;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\messenger\Chat;
+use Illuminate\Support\Facades\Log;
+
+class ChatController extends Controller
+{
+    
+}
