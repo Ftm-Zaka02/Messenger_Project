ed8cdac803270cd387503555791f4bb48c8c5541
Fateme Zaka <ftmzaka.pr02@gmail.com>
2024-04-10 10:55:52 +0330

    feat: write getChat method controller

--- app/Http/Controllers/messenger/ChatController.php
+++ app/Http/Controllers/messenger/ChatController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers\messenger;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\messenger\Chat;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 
 class ChatController extends Controller
 {
-
+    public static function get()
+    {
+        try {
+            $model = Chat::getChat();
+            $response = json_encode([
+                'status' => 'success',
+                'data' => $model
+            ]);
+            return response($response, 200);
+        } catch (\Exception $error) {
+            Log::error('getting chat got error: ' . $error->getMessage());
+            $response = json_encode([
+                'status' => 'error',
+                'message' => $error->getMessage(),
+            ]);
+            return response($response, 500);
+        }
+    }
 }
